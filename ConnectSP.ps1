5393718eab9c9970389e4677726f34bfdce7b86b
JCastilloSurgicorp <jdcastillo@appsurgicorp.onmicrosoft.com>
2024-08-08 18:20:12 -0500

    deleting 1000 records

--- ConnectSP.ps1
+++ ConnectSP.ps1
@@ -16,7 +16,7 @@ $spQuery = "<View>
                     <Where>
                         <Gt>
                             <FieldRef Name='ID'/>
-                            <Value Type='Number'>37761</Value>
+                            <Value Type='Number'>39560</Value>
                         </Gt>
                     </Where>
                 </Query>
